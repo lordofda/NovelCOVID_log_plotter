83c1da20e088683ced7c37aab60d4fc6ad7145f8
lordofda <lordofda@gmail.com>
2020-04-08 11:29:05 +0200

    Comments and labels

--- corona2.py
+++ corona2.py
@@ -4,34 +4,30 @@ import pandas
 import matplotlib.pyplot as plt
 import argparse
 
+#Parse argument -c
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--country", help="Country to plot the data for, see https://github.com/NovelCOVID/API", type=str, default="Poland")
 args = parser.parse_args()
 
+#Request data
 req = Request('https://corona.lmao.ninja/v2/historical/{}'.format(args.country), headers={'User-Agent': 'Mozilla/5.0'})
 data = urlopen(req).read()
 output = json.loads(data)
 
-
-#print(output['timeline']['cases'])
+#Make panda dataframe
 df = pandas.DataFrame(output['timeline']['cases'], index=[0])
 df = df.transpose()
 df.columns = ['A']
+
+#Calculate new cases
 df['B'] = df['A'] - df['A'].shift(1)
 
-
-
-#fig = df.plot.line(x='A', y='B', loglog=True).get_figure()
-#fig.savefig('test.pdf')
-#print(df)
-#df.plot()
-
-#df.ewm(alpha=0.5).mean().plot.line(style= 'r--' x='A', y='B', loglog=True, label = 'Smoothed')
-#df.plot.line(x='A', y='B', loglog=True, label = 'Raw')
+#Make a plot
 plt.title('Corona trend for {}'.format(args.country))
 plt.xscale('log')
 plt.yscale('log')
-#plt.plot('A', 'B', '.--g', data=df.ewm(alpha=0.8).mean())
+plt.xlabel('Total Cases')
+plt.ylabel('New Cases')
 plt.plot('A', 'B', '.-g', data=df.ewm(alpha=0.3).mean(), label="Smoothed")
 plt.plot('A', 'B', '.--b', data=df, label="Raw")
 plt.legend()
